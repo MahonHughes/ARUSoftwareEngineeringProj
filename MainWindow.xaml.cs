babe463187ad682b89d1298a31ef392a799e6e23
Kvasir3000 <56788564+Kvasir3000@users.noreply.github.com>
2020-10-28 04:23:06 +0300

    Add files via upload
    Code for the login page

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace LoginPage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Submit_Button_Click(object sender, RoutedEventArgs e)
+        {
+            DBConnection.GetInstance();
+            bool flag = UserAuthorization.Verification(this.Name_Text_Box.Text, this.Password_Text_Box.Text);
+            if (flag)
+            {
+                MainPage mainPage = new MainPage();
+                mainPage.Show();
+                this.Hide();
+            }
+            else
+            {
+                System.Windows.MessageBox.Show("Invalid data");
+            }
+           
+
+        }
+    }
+}
---
+++ UserAuthorization.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginPage
+{
+    /// <summary>
+    /// Child class of the DBConnection
+    /// </summary>
+    public class UserAuthorization: DBConnection
+    {
+      
+        /// <summary>
+        /// Takes the data, from the user input and checks it in the database 
+        /// </summary>
+        /// <param name="data"> can be either name of the user or the password </param>
+        /// <param name="sqlCmd">can be either command for password or for the </param>
+        /// <returns></returns>
+        private static bool SearchData(string data, string sqlCmd)
+        {
+            bool result = false;
+            using (dbConnetion = new SqlConnection (connString))
+            {
+                dbConnetion.Open();
+                SqlCommand cmd = new SqlCommand(sqlCmd,dbConnetion);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    if (reader[0].ToString() == data)
+                    {
+                        result = true;
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+      
+        public static bool Verification(string name, string password)
+        {
+            bool result = false;
+            bool flag1 = SearchData(name, "select user_name from [Users]");
+            bool flag2 = SearchData(password, "select user_password from [Users]");
+            if (flag1 && flag2)
+                result = true;
+            return result;
+        }
+       
+    }
+}
