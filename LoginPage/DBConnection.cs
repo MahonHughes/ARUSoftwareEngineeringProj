cf0b3a3df258b6088758e2595ef2182ef860b906
Luke Burdon <lb1251@student.aru.ac.uk>
2020-11-01 17:22:59 +0000

    Nikita's Version As Test

---
+++ LoginPage/DBConnection.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginPage
+{
+    public class DBConnection
+    {
+        // variable to connect program to the database
+        protected static string connString;
+
+        // variable to get interact with the DB
+        protected static SqlConnection dbConnetion;
+
+        // object of this class 
+        private static DBConnection instance;
+
+        /// <summary>
+        /// Constructor 
+        /// </summary>
+        protected DBConnection()
+        {
+            connString = Properties.Settings.Default.userDBconnStr;
+        }
+ 
+        public static DBConnection GetInstance()
+        {
+            if (instance == null)
+                instance = new DBConnection();
+            return instance;
+        }
+        
+
+    }
+}
