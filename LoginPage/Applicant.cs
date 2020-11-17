88d1d7dd7725e6f08d1acc26e5a4e3a6feafd9fa
MahonHughes <34442699+MahonHughes@users.noreply.github.com>
2020-11-17 14:27:28 +0000

    Added email address to Applicant class

--- LoginPage/Applicant.cs
+++ LoginPage/Applicant.cs
@@ -9,11 +9,13 @@ namespace LoginPage
     public class Applicant
     {
         public string name;
+        public string emailAddress;
         public int iD;
 
-        public Applicant(string _name, int _iD)
+        public Applicant(string _name, string _emailAddress, int _iD)
         {
             name = _name;
+            emailAddress = _emailAddress;
             iD = _iD;
         }
     }
